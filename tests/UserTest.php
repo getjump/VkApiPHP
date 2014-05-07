77ba996479d8093595501f82ff0cd1c48d15e5c5
Pavel Savinov <getjump0@gmail.com>
2014-05-07 17:55:44 +1100

    fix

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -14,8 +14,7 @@ class UserTest extends PHPUnit_Framework_TestCase {
         $vk = new \getjump\Vk\Core();
         $user = new \getjump\Vk\Wrapper\User($vk->apiVersion('5.21'));
         $obj = $user->get(1)->response->data[0];
-        $this->assertInstanceOf('\getjump\Vk\Models\User', $obj);
+        $this->assertInstanceOf('\getjump\Vk\Model\User', $obj);
         $this->assertEquals(1, $obj->id);
-        //$this->assertObjectHasAttribute('first_name', $obj);
     }
 }
