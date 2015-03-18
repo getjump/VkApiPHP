147ddaea0eb0c070fc8bec9e5f62465a7527df97
Nikita Sapogov <amstaffix@gmail.com>
2015-03-18 14:10:29 +0300

    Add documentation to Response/Auth

--- src/getjump/Vk/Response/Auth.php
+++ src/getjump/Vk/Response/Auth.php
@@ -2,17 +2,27 @@
 
 namespace getjump\Vk\Response;
 
-
-class Auth {
-
+/**
+ * Structure of auth token
+ */
+class Auth
+{
+    /** @var string token content */
     public $token = false;
+    /** @var int time of life in seconds */
     public $expiresIn = false;
+    /** @var int authorized user ID */
     public $userId = false;
 
+    /**
+     * @param bool|string $token token content
+     * @param bool|int $expiresIn time of life in seconds
+     * @param bool|int $userId authorized user ID
+     */
     public function __construct($token = false, $expiresIn = false, $userId = false)
     {
         $this->token = $token;
         $this->expiresIn = $expiresIn;
         $this->userId = $userId;
     }
-} 
+}
