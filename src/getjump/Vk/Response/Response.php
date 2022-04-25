6722a56fab4228dec315891a1df249b3f74500ed
Konstantin <obuchov.k@gmail.com>
2022-04-25 21:09:43 +0300

    Update Response.php
    Count on stdClass fails fix it

--- src/getjump/Vk/Response/Response.php
+++ src/getjump/Vk/Response/Response.php
@@ -56,7 +56,7 @@ class Response implements \ArrayAccess, \Countable, \Iterator
         }
         $this->count = !isset($data->count) ? false : $data->count;
         if (is_array($data) || !isset($data->items)) {
-            $this->count = count($data);
+            $this->count = count((array) $data);
             if (is_array($data) && is_callable($callback)) {
                 foreach ($data as $d) {
                     $this->data[] = call_user_func_array($callback, [$d]);
